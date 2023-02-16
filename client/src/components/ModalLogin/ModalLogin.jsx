e4266fdef72a8b679e648704006c783552a7d43d
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-02-16 20:17:23 -0300

    modal login navbar

---
+++ client/src/components/ModalLogin/ModalLogin.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import FormLogin from "../FormLogin/FormLogin";
+import style from "./ModaLogin.module.css";
+import CloseButton from "../ModalInscripcion/svg/CloseButton";
+import { useSelector } from "react-redux";
+
+export default function ModalLogin({handlerClose}) {
+  const { isAuth } = useSelector((state) => state.auth);
+//   const [Open, setOpen] = useState(false);
+  const toggleClose = () => {
+    handlerClose(false)
+  };
+  return (
+    <div className={style.Overlay}>
+      <div style={{ paddingTop: "40px", marginBottom: "20px" }}>
+        <div className={style.contenedorModal}>
+          <div className={style.DivCloseButton}>
+            <div style={{cursor:'pointer'}}  onClick={toggleClose}>
+              <CloseButton />
+            </div>
+          </div>
+          {isAuth === true ? "Ya estas logueado" : <FormLogin/>}
+        </div>
+      </div>
+    </div>
+  );
+}
