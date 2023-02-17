e5c4a2a84563e10bb592ebb0e4520d0ecfab6fdb
Robmargar <roberto.mar.garc@gmail.com>
2023-02-17 09:17:47 -0600

    Agregados galeria, foto_inicial- url video a Colegios, Creado endpoint categoria y relacion con Colegio

---
+++ api/src/routes/categoria.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const router = Router();
+const { Categoria } = require("../db.js");
+
+//------- PEDIR TODOS LAS CATEGORIAS A LA BD--------
+router.get("/", async (req, res) => {
+  let response = [];
+  try {
+    let categoria;
+    categoria = await Categoria.findAll({
+      attributes: [
+        "id",
+        "nombre_categoria",
+        "imagen_categoria",
+        "logo_categoria",
+      ],
+    });
+
+    res.json(categoria);
+  } catch (err) {
+    res.json({ err });
+  }
+});
+
+module.exports = router;
