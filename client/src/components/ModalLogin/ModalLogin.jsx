fb7ae3d88f859c2b42c859f20e67c13ec6cd948f
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-02-20 19:36:19 -0300

    Login bienvenida

--- client/src/components/ModalLogin/ModalLogin.jsx
+++ client/src/components/ModalLogin/ModalLogin.jsx
@@ -1,25 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FormLogin from "../FormLogin/FormLogin";
 import style from "./ModaLogin.module.css";
 import CloseButton from "../ModalInscripcion/svg/CloseButton";
 import { useSelector } from "react-redux";
+import Logo from "../../assets/logoPayment.png";
 
-export default function ModalLogin({handlerClose}) {
+export default function ModalLogin({ handlerClose }) {
   const { isAuth } = useSelector((state) => state.auth);
-//   const [Open, setOpen] = useState(false);
+  //   const [Open, setOpen] = useState(false);
   const toggleClose = () => {
-    handlerClose(false)
+    handlerClose(false);
   };
+
+  useEffect(() => {}, [isAuth]);
+
   return (
     <div className={style.Overlay}>
       <div>
         <div className={style.contenedorModal}>
           <div className={style.DivCloseButton}>
-            <div style={{cursor:'pointer'}}  onClick={toggleClose}>
+            <div style={{ cursor: "pointer" }} onClick={toggleClose}>
               <CloseButton />
             </div>
           </div>
-          {isAuth === true ? "Ya estas logueado" : <FormLogin/>}
+          {isAuth === true ? (
+            <>
+              <div style={{display:'flex', flexDirection:'column'}}>
+                <div className={style.img_div}>
+                  <img src={Logo} alt="logo" />
+                  </div>
+                  <div className={style.content_div}>
+                    <h1>Bienvenido a MiCole </h1>
+                    <p>El inicio de sesion ha sido exitoso!</p>
+                    <button onClick={toggleClose}>Continuar</button>
+                  </div>
+                </div>
+            
+            </>
+          ) : (
+            <FormLogin />
+          )}
         </div>
       </div>
     </div>
