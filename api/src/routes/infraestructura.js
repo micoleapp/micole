f00a24c3fc45ff799c0199d89bec5adb4dd11737
Robmargar <roberto.mar.garc@gmail.com>
2023-02-25 08:06:28 -0600

    Ya se pueden guardar arreglos de infraestructura y de Categoria para los Colegios

--- api/src/routes/infraestructura.js
+++ api/src/routes/infraestructura.js
@@ -1,13 +1,18 @@
 const { Router } = require("express");
 const router = Router();
-const { Infraestructura } = require("../db.js");
+const { Infraestructura, Infraestructura_tipo } = require("../db.js");
 
 //------- PEDIR TODOS LAS INFRAESTRUCTURAS A LA BD--------
 router.get("/", async (req, res) => {
-
   try {
     let infraestructura;
     infraestructura = await Infraestructura.findAll({
+      include: [
+        {
+          model: Infraestructura_tipo,
+          attributes: ["infraestructura_tipo"],
+        },
+      ],
       attributes: [
         "id",
         "nombre_infraestructura",
