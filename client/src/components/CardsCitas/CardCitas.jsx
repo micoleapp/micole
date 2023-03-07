980f02e04939da03390c5d7e8ac2a141f1549d59
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-03-07 11:42:19 -0300

    citas agendadas put

--- client/src/components/CardsCitas/CardCitas.jsx
+++ client/src/components/CardsCitas/CardCitas.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import style from "./CardCita.module.css";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import PersonPinIcon from "@mui/icons-material/PersonPin";
@@ -8,10 +8,19 @@ import EventIcon from "@mui/icons-material/Event";
 import PhoneIcon from "@mui/icons-material/Phone";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import { Button } from "@mui/material";
+import { putCita } from "../../redux/CitasActions";
 export default function CardCitas() {
   const { citasAgendadas } = useSelector((state) => state.schools);
+  const [idCita, setidCita] = useState("");
+  const [Estado, setEstado] = useState("");
   console.log(citasAgendadas);
-
+  
+  const dispatch =useDispatch()
+  const handlerPutStateCita = () => {
+    console.log(idCita);
+  const estado = "Pendiente"
+    dispatch(putCita(idCita,estado))
+  };
   return (
     <>
       <div className={style.layout}>
@@ -20,58 +29,68 @@ export default function CardCitas() {
             return (
               <>
                 <div className={style.container}>
-              
                   <div
                     style={{
                       display: "flex",
                       gap: "5px",
                       flexDirection: "column",
-                      width:'100%',
-                      fontSize:'1.8vh'
+                      width: "100%",
+                      fontSize: "1.8vh",
                     }}
                   >
-                  
-                    <div style={{ display: "flex", gap: "10px" , flexDirection:'row',alignItems:'center',   fontSize:'1.8vh'}}>
-                    <img
-                    style={{ width: "50px", height: "50px" }}
-                    src="https://res.cloudinary.com/dj8p0rdxn/image/upload/v1676414550/xuj9waxpejcnongvhk9o.png"
-                    alt=""
-                  />
-                  <div >
-                    <div className={style.divNombreGrado} >
-                           <p>{cita.nombre}</p>
-                      {/* cita.grado */} <p>1ro - Primaria</p>
-                    </div>
-                      
-                      <div className={style.itemDiv} >
-                      <div  className={style.itemDiv} >
-                        <AccessTimeIcon
-                          style={{
-                            width: "20px",
-                            height: "20px",
-                            color: "grey",
-                          }}
-                        />
-                        <p>{cita.hora_cita}</p>
-                      </div>{" "}
-                      <div  className={style.itemDiv} >
-                        <EventIcon
-                          style={{
-                            width: "20px",
-                            height: "20px",
-                            color: "grey",
-                          }}
-                        />{" "}
-                        <p>{cita.fecha_cita}</p>
+                    <div
+                      style={{
+                        display: "flex",
+                        gap: "10px",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        fontSize: "1.8vh",
+                      }}
+                    >
+                      <img
+                        style={{ width: "50px", height: "50px" }}
+                        src="https://res.cloudinary.com/dj8p0rdxn/image/upload/v1676414550/xuj9waxpejcnongvhk9o.png"
+                        alt=""
+                      />
+                      <div>
+                        <div className={style.divNombreGrado}>
+                          <p>{cita.nombre}</p>
+                          {/* cita.grado */} <p>1ro - Primaria</p>
+                        </div>
+
+                        <div className={style.itemDiv}>
+                          <div className={style.itemDiv}>
+                            <AccessTimeIcon
+                              style={{
+                                width: "20px",
+                                height: "20px",
+                                color: "grey",
+                              }}
+                            />
+                            <p>{cita.hora_cita}</p>
+                          </div>{" "}
+                          <div className={style.itemDiv}>
+                            <EventIcon
+                              style={{
+                                width: "20px",
+                                height: "20px",
+                                color: "grey",
+                              }}
+                            />{" "}
+                            <p>{cita.fecha_cita}</p>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </div>
-                 
-                    </div>
-
-                   
-                  </div>
-                  <div style={{ display: "flex", gap: "5px", width:'100%', justifyContent:'center' }}>
+                  <div
+                    style={{
+                      display: "flex",
+                      gap: "5px",
+                      width: "100%",
+                      justifyContent: "center",
+                    }}
+                  >
                     {cita.modalidad === "Virtual" && (
                       <LaptopWindowsIcon
                         style={{
@@ -92,22 +111,39 @@ export default function CardCitas() {
                     )}
                     <p>{cita.modalidad}</p>
                   </div>
-                  <div style={{ display: "flex", gap: "5px", width:'100%', justifyContent:'center', flexDirection:'column' }}>
-                  <div  className={style.itemDiv} >
-                        <PhoneIcon 
-                          style={{
-                            width: "20px",
-                            height: "20px",
-                            color: "grey",
-                          }}
-                        />
-                       <p>{cita.telefono}</p>
-                      </div>
-                   
+                  <div
+                    style={{
+                      display: "flex",
+                      gap: "5px",
+                      width: "100%",
+                      justifyContent: "center",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <div className={style.itemDiv}>
+                      <PhoneIcon
+                        style={{
+                          width: "20px",
+                          height: "20px",
+                          color: "grey",
+                        }}
+                      />
+                      <p>{cita.telefono}</p>
+                    </div>
+
                     <p>{cita.email}</p>
                   </div>
                   <div>
-                    <Button variant="contained">Confirmar </Button>
+                    <Button
+                      onClick={(event) => {
+                        handlerPutStateCita();
+                        setidCita(cita.id);
+                        // setEstado(cita.estado);
+                      }}
+                      variant="contained"
+                    >
+                      Confirmar{" "}
+                    </Button>
                   </div>
                 </div>
               </>
--- client/src/redux/CitasActions.js
+++ client/src/redux/CitasActions.js
@@ -1,13 +1,33 @@
 import axios from "axios";
-import { getCitas } from "./CitasSlice";
+import { getCitas, getError, isLoading } from "./CitasSlice";
 
 export const getCita = () => (dispatch) => {
-
+  dispatch(isLoading());
   const token = localStorage.getItem("token");
   axios
     .get(`/citas`, { headers: { Authorization: `Bearer ${token}` } })
     .then((res) => dispatch(getCitas(res.data)))
     .catch((err) => {
-   console.log(err)
+      dispatch(getError(err.response.data.error));
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: err.response.data.error,
+      });
     });
 };
+
+export const putCita = ({ idCita, estado }) =>(dispatch) => {
+    dispatch(isLoading());
+    axios
+      .put(`/citas/${idCita}`, { estado:'Pendiente'})
+      // .then((res) => dispatch(getCitas(res.data)))
+      .catch((err) => {
+        dispatch(getError(err.response.data.error));
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: err.response.data.error,
+        });
+      });
+  };
