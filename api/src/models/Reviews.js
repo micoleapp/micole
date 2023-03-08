aae0627521d891ac58f71d99207917cf76f08dac
Maximo Solis Farfan <maxsolfar@gmail.com>
2023-03-08 16:41:50 -0500

    Modelo Review y Router

--- api/src/models/Reviews.js
+++ api/src/models/Reviews.js
@@ -11,9 +11,34 @@ module.exports = (sequelize) => {
       primaryKey: true,
     },
     rating: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.FLOAT,
     },
-    comment: {
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      validate: {
+        is: {
+          args: /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/,
+          msg: "Nombre solo puede contener letras, acentos, tildes y espacios",
+        },
+        len: {
+          args: [4, 60],
+          msg: 'Nombre debe tener entre 4 y 60 letras',
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "Ingresa un email valido",
+        },
+      },
+    },
+    comentario: {
       type: DataTypes.STRING,
     }
   }, {
