95a8c5f44626a3b2cf941e222f87077084bcc276
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-03-13 15:31:18 -0300

    update card citas

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const insertDistritos = require("./src/utils/insertDistritos");
 
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   insertDistritos();
   server.listen(process.env.PORT, () => {
     console.log(`⇒ listening at port ${process.env.PORT}`);
