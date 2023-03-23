30ba4484c12ddea56627ddd94aff8cafe359cff0
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-03-23 10:35:17 -0300

    paginado de colegios

--- client/src/pages/Admin/pageColegios/PageColegio.jsx
+++ client/src/pages/Admin/pageColegios/PageColegio.jsx
@@ -1,29 +1,53 @@
+import { Box, Pagination } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import PaginationCitas from "../../../components/CardsCitas/Paginacion/PaginationCitas";
-import sliceIntoChunks from "../../../components/CardsCitas/Paginacion/utils/SliceCitas";
-import { getAllSchools } from "../../../redux/SchoolsActions";
+import { getAllSchoolsPage } from "../../../redux/SchoolsActions";
+
 import CardColegio from "./card-colegio-admin/CardColegio";
 import SearchCoelegio from "./search-colegio-admin/SearchCoelegio";
 export default function PageColegio() {
   const dispatch = useDispatch();
-  const { allschools, loading } = useSelector((state) => state.schools);
+  const [page, setPage] = React.useState(1);
+  const { allschools, pagination, loading } = useSelector(
+    (state) => state.schools
+  );
 
   const [Input, setInput] = useState("");
- 
-
+  useEffect(() => {
+    dispatch(getAllSchoolsPage(page));
+  }, [page]);
   console.log(Input);
-
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+  console.log(pagination.pages);
   return (
     <>
-      {/* searh colegio */}
-
       <div style={{ display: "flex", flexDirection: "column", gap: "3vh" }}>
-        <SearchCoelegio handlerInput={setInput}   nroColegios={allschools?.length} data={ allschools&& allschools} />
-        <CardColegio input={Input} data={allschools&& allschools} isLoading={loading&& loading} />
+        <SearchCoelegio
+          handlerInput={setInput}
+          nroColegios={allschools?.length}
+          data={allschools && allschools}
+        />
+        <CardColegio
+          input={Input}
+          data={allschools && allschools}
+          isLoading={loading && loading}
+        />
       </div>
-
-   
+      <Box
+        justifyContent={"start"}
+        alignItems={"center"}
+        display={"flex"}
+        sx={{ margin: "20px 0px" }}
+      >
+        <Pagination
+          count={pagination.pages}
+          onChange={handlePageChange}
+          page={page}
+          color="primary"
+        />
+      </Box>
     </>
   );
 }
--- client/src/pages/Admin/pageColegios/card-colegio-admin/CardColegio.jsx
+++ client/src/pages/Admin/pageColegios/card-colegio-admin/CardColegio.jsx
@@ -6,10 +6,12 @@ import style from "./cardColegio.module.css";
 import Swal from "sweetalert2";
 import axios from "axios";
 import { getAllSchools } from "../../../../redux/SchoolsActions";
-import LoadingButton from "@mui/lab/LoadingButton";
+
+import ContentLoader from "react-content-loader";
 export default function CardColegio({ input, data, isLoading }) {
   const dispatch = useDispatch();
-  console.log(input, data);
+  const items = [1, 2, 3, 4, 5];
+
   const putActiveColegio = (id) => {
     console.log(id);
     try {
@@ -82,7 +84,6 @@ export default function CardColegio({ input, data, isLoading }) {
             >
               {/* {Toggle === false ? "Desactivar" : "Activar"} */}
               Desactivar
-       
             </Button>
           </div>
         )}
@@ -102,101 +103,119 @@ export default function CardColegio({ input, data, isLoading }) {
     );
   }
 
-  useEffect(() => {}, [data]);
 
   return (
     <>
       <HeadTable />
       <div>
-        {data &&
-          data?.map((ele) => {
-            let str = ele.direccion;
+        {data?.length > 0 &&isLoading===false
+          ? data?.map((ele) => {
+              let str = ele.direccion;
 
-            console.log(str);
-            let str2 = str?.slice(0, 60);
-            console.log(str2);
-            return (
-              <>
-                <div className={style.layout}>
-                  {/* foto mas info */}
-                  <div className={style.imgInfoDiv}>
-                    <img
-                      className="object-cover w-40 h-40"
-                      src={ele.primera_imagen}
-                    />
+              console.log(str);
+              let str2 = str?.slice(0, 60);
+              console.log(str2);
+              return (
+                <>
+                  <div className={style.layout}>
+                    {/* foto mas info */}
+                    <div className={style.imgInfoDiv}>
+                      <img
+                        className="object-cover w-40 h-40"
+                        src={ele.primera_imagen}
+                      />
 
-                    <div>
-                      <Typography
-                        sx={{
-                          padding: "1vh",
-                          color: "#0D263B",
-                          fontWeight: "600",
-                          fontSize: "2.4vh",
-                        }}
-                        id="modal-modal-title"
-                        variant="h6"
-                        component="h2"
-                      >
-                        {ele.nombre_colegio}
-                      </Typography>
-                      <Typography
-                        sx={{
-                          padding: "1vh",
-                          color: "#0D263B",
-                          fontWeight: "600",
-                          fontSize: "1.5vh",
-                        }}
-                      >
-                        <div
-                          style={{
-                            display: "flex",
-                            gap: "1vh",
+                      <div>
+                        <Typography
+                          sx={{
+                            padding: "1vh",
+                            color: "#0D263B",
+                            fontWeight: "600",
+                            fontSize: "2.4vh",
+                          }}
+                          id="modal-modal-title"
+                          variant="h6"
+                          component="h2"
+                        >
+                          {ele.nombre_colegio}
+                        </Typography>
+                        <Typography
+                          sx={{
+                            padding: "1vh",
+                            color: "#0D263B",
+                            fontWeight: "600",
+                            fontSize: "1.5vh",
+                          }}
+                        >
+                          <div
+                            style={{
+                              display: "flex",
+                              gap: "1vh",
+                            }}
+                          >
+                            <div className={style.divTypo}>
+                              <b style={{ color: "#0061DF" }}>Dirección</b>
+                              <p>{str2}</p>
+                            </div>
+                          </div>
+                        </Typography>
+                        <Typography
+                          sx={{
+                            padding: "1vh",
+                            color: "#0D263B",
+                            fontWeight: "600",
+                            fontSize: "1.5vh",
                           }}
                         >
                           <div className={style.divTypo}>
-                            <b style={{ color: "#0061DF" }}>Dirección</b>
-                            <p>{str2}</p>
+                            <b style={{ color: "#0061DF" }}>Teléfono</b>
+                            <p>{ele.telefono}</p>
                           </div>
-                        </div>
-                      </Typography>
-                      <Typography
-                        sx={{
-                          padding: "1vh",
-                          color: "#0D263B",
-                          fontWeight: "600",
-                          fontSize: "1.5vh",
-                        }}
-                      >
-                        <div className={style.divTypo}>
-                          <b style={{ color: "#0061DF" }}>Teléfono</b>
-                          <p>{ele.telefono}</p>
-                        </div>
-                      </Typography>
+                        </Typography>
+                      </div>
                     </div>
-                  </div>
 
-                  {/*  fecha */}
-                  <div className={style.itemDiv}>
-                    <p>12 de abril del 2023</p>
-                  </div>
-                  {/*  plan */}
-                  <div className={style.itemDiv}>
-                    {/* //   Plan_Pago: { id: 1, nombre_plan_pago: 'Free' }, */}
-                    <p> {ele.Plan_Pago.nombre_plan_pago}</p>
-                  </div>
-                  {/* boton */}
+                    {/*  fecha */}
+                    <div className={style.itemDiv}>
+                      <p>12 de abril del 2023</p>
+                    </div>
+                    {/*  plan */}
+                    <div className={style.itemDiv}>
+                      {/* //   Plan_Pago: { id: 1, nombre_plan_pago: 'Free' }, */}
+                      <p> {ele.Plan_Pago.nombre_plan_pago}</p>
+                    </div>
+                    {/* boton */}
 
-                  <ActDesButton
-                    // onClick={() => {
-                    //   putActiveColegio(ele.id);
-                    // }}
-                    isActive={ele.isActive}
-                    id={ele.id}
-                  />
-                </div>
-              </>
-            );
-          })}
+                    <ActDesButton
+                      // onClick={() => {
+                      //   putActiveColegio(ele.id);
+                      // }}
+                      isActive={ele.isActive}
+                      id={ele.id}
+                    />
+                  </div>
+                </>
+              );
+            })
+          : items.map((item, key) => (
+              <ContentLoader
+                key={key}
+                speed={3}
+                width={"100%"}
+                height={"100%"}
+                viewBox="0 0 500 120"
+                backgroundColor="#dcdce2"
+                foregroundColor="#ecebeb"
+              >
+                <rect x="110" y="8" rx="3" ry="3" width="120" height="10" />
+                <rect x="110" y="25" rx="3" ry="3" width="100" height="6" />
+                <rect x="48" y="26" rx="3" ry="3" width="52" height="6" />
+                <rect x="110" y="56" rx="3" ry="3" width="310" height="6" />
+                <rect x="110" y="72" rx="3" ry="3" width="300" height="6" />
+                <rect x="110" y="88" rx="3" ry="3" width="178" height="6" />
+                <rect width="100" height="100" />
+              </ContentLoader>
+            ))}
       </div>
     </>
   );
