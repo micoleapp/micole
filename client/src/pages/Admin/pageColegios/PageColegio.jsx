9c643affd8f5c92029b32869b90b1b738dae284e
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-03-23 02:01:00 -0300

    funcionalidad act desc

--- client/src/pages/Admin/pageColegios/PageColegio.jsx
+++ client/src/pages/Admin/pageColegios/PageColegio.jsx
@@ -7,14 +7,13 @@ import CardColegio from "./card-colegio-admin/CardColegio";
 import SearchCoelegio from "./search-colegio-admin/SearchCoelegio";
 export default function PageColegio() {
   const dispatch = useDispatch();
-  const { allschools } = useSelector((state) => state.schools);
+  const { allschools, loading } = useSelector((state) => state.schools);
   const [colegio, setColegio] = useState([]);
   const [page, setPage] = React.useState(0);
   const [Input, setInput] = useState("");
   useEffect(() => {
-    dispatch(getAllSchools());
-    let paginaColegios = sliceIntoChunks( allschools&&allschools, 5);
-    setColegio(paginaColegios);
+    // dispatch(getAllSchools());
+    setColegio(allschools)
   }, []);
   console.log(colegio);
   console.log(Input);
@@ -25,14 +24,14 @@ export default function PageColegio() {
 
       <div style={{ display: "flex", flexDirection: "column", gap: "3vh" }}>
         <SearchCoelegio handlerInput={setInput}   nroColegios={allschools?.length} data={ colegio&& colegio} />
-        <CardColegio input={Input} data={colegio&& colegio[page]} />
+        <CardColegio input={Input} data={allschools&& allschools} isLoading={loading&& loading} />
       </div>
 
-      <PaginationCitas
+      {/* <PaginationCitas
         nroPaginas={colegio?.length}
         page={page}
         setPage={setPage}
-      />
+      /> */}
     </>
   );
 }
--- client/src/pages/Admin/pageColegios/card-colegio-admin/CardColegio.jsx
+++ client/src/pages/Admin/pageColegios/card-colegio-admin/CardColegio.jsx
@@ -1,28 +1,29 @@
 import { Button, Card, Typography } from "@mui/material";
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import HeadTable from "./headTable/HeadTable";
 import style from "./cardColegio.module.css";
 import Swal from "sweetalert2";
 import axios from "axios";
-export default function CardColegio({ input, data }) {
+import { getAllSchools } from "../../../../redux/SchoolsActions";
+import LoadingButton from "@mui/lab/LoadingButton";
+export default function CardColegio({ input, data, isLoading }) {
+  const dispatch = useDispatch();
   console.log(input, data);
-
-  const [success, setSuccess] = useState(false);
   const putActiveColegio = (id) => {
     console.log(id);
     try {
       axios
         .put(`/colegios/activo/${id}`, { isActive: true })
         .then((res) => {
-          setSuccess(true);
-          Swal.fire("Exito", "Datos actualizados", "success");
+          dispatch(getAllSchools());
+          // Swal.fire("Exito", "Datos actualizados", "success");
         })
         .catch((err) => {
           Swal.fire({
             icon: "error",
             title: "Algo salio mal",
-            text: err.response.data.error,
+            text: err,
           });
         });
     } catch (error) {
@@ -39,8 +40,8 @@ export default function CardColegio({ input, data }) {
       axios
         .put(`/colegios/activo/${id}`, { isActive: false })
         .then((res) => {
-          setSuccess(true);
-          Swal.fire("Exito", "Datos actualizados", "success");
+          dispatch(getAllSchools());
+          // Swal.fire("Exito", "Datos actualizados", "success");
         })
         .catch((err) => {
           Swal.fire({
@@ -57,118 +58,148 @@ export default function CardColegio({ input, data }) {
       });
     }
   };
+
+  function ActDesButton({ isActive, id }) {
+    const [Toggle, setToggle] = useState(false);
+ 
+    const toggleBtn = () => {
+      setToggle(true);
+      if (isActive === true) {
+        putDesactiveColegio(id);
+      } else {
+        putActiveColegio(id);
+      }
+    };
+
+    return (
+      <>
+        {isActive === true && (
+          <div className={style.itemDiv}>
+            {/* {isLoading && <div className={style.loader}></div>} */}
+           
+              <Button
+                onClick={toggleBtn}
+                variant="outlined"
+                sx={{ fontFamily: "Poppins", fontSize: "1.4vh" }}
+              >
+                {Toggle === false ? "Desactivar" : "Activar"}
+              </Button>
+            
+          </div>
+        )}
+        {isActive === false && (
+          <div className={style.itemDiv}>
+            {/* {isLoading && <div className={style.loader}></div>} */}
+          
+              <Button
+                variant="outlined"
+                sx={{ fontFamily: "Poppins", fontSize: "1.4vh" }}
+                onClick={toggleBtn}
+              >
+                {Toggle === false ? "Activar" : "Desactivar"}
+              </Button>
+          
+          </div>
+        )}
+      </>
+    );
+  }
+
+  // useEffect(() => {}, [data]);
+
   return (
     <>
       <HeadTable />
       <div>
-        {data?.map((ele) => {
-          let str = ele.direccion;
+        {data &&
+          data?.map((ele) => {
+            let str = ele.direccion;
 
-          console.log(str);
-          let str2 = str?.slice(0, 60);
-          console.log(str2);
-          return (
-            <>
-              <div className={style.layout}>
-                {/* foto mas info */}
-                <div className={style.imgInfoDiv}>
-                  <img
-                    className="object-cover w-40 h-40"
-                    src={ele.primera_imagen}
-                  />
+            console.log(str);
+            let str2 = str?.slice(0, 60);
+            console.log(str2);
+            return (
+              <>
+                <div className={style.layout}>
+                  {/* foto mas info */}
+                  <div className={style.imgInfoDiv}>
+                    <img
+                      className="object-cover w-40 h-40"
+                      src={ele.primera_imagen}
+                    />
 
-                  <div>
-                    <Typography
-                      sx={{
-                        padding: "1vh",
-                        color: "#0D263B",
-                        fontWeight: "600",
-                        fontSize: "2.4vh",
-                      }}
-                      id="modal-modal-title"
-                      variant="h6"
-                      component="h2"
-                    >
-                      {ele.nombre_colegio}
-                    </Typography>
-                    <Typography
-                      sx={{
-                        padding: "1vh",
-                        color: "#0D263B",
-                        fontWeight: "600",
-                        fontSize: "1.5vh",
-                      }}
-                    >
-                      <div
-                        style={{
-                          display: "flex",
-                          gap: "1vh",
+                    <div>
+                      <Typography
+                        sx={{
+                          padding: "1vh",
+                          color: "#0D263B",
+                          fontWeight: "600",
+                          fontSize: "2.4vh",
+                        }}
+                        id="modal-modal-title"
+                        variant="h6"
+                        component="h2"
+                      >
+                        {ele.nombre_colegio}
+                      </Typography>
+                      <Typography
+                        sx={{
+                          padding: "1vh",
+                          color: "#0D263B",
+                          fontWeight: "600",
+                          fontSize: "1.5vh",
+                        }}
+                      >
+                        <div
+                          style={{
+                            display: "flex",
+                            gap: "1vh",
+                          }}
+                        >
+                          <div className={style.divTypo}>
+                            <b style={{ color: "#0061DF" }}>Dirección</b>
+                            <p>{str2}</p>
+                          </div>
+                        </div>
+                      </Typography>
+                      <Typography
+                        sx={{
+                          padding: "1vh",
+                          color: "#0D263B",
+                          fontWeight: "600",
+                          fontSize: "1.5vh",
                         }}
                       >
                         <div className={style.divTypo}>
-                          <b style={{ color: "#0061DF" }}>Dirección</b>
-                          <p>{str2}</p>
+                          <b style={{ color: "#0061DF" }}>Teléfono</b>
+                          <p>{ele.telefono}</p>
                         </div>
-                      </div>
-                    </Typography>
-                    <Typography
-                      sx={{
-                        padding: "1vh",
-                        color: "#0D263B",
-                        fontWeight: "600",
-                        fontSize: "1.5vh",
-                      }}
-                    >
-                      <div className={style.divTypo}>
-                        <b style={{ color: "#0061DF" }}>Teléfono</b>
-                        <p>{ele.telefono}</p>
-                      </div>
-                    </Typography>
+                      </Typography>
+                    </div>
                   </div>
-                </div>
 
-                {/*  fecha */}
-                <div className={style.itemDiv}>
-                  <p>12 de abril del 2023</p>
-                </div>
-                {/*  plan */}
-                <div className={style.itemDiv}>
-                  {/* //   Plan_Pago: { id: 1, nombre_plan_pago: 'Free' }, */}
-                  <p> {ele.Plan_Pago.nombre_plan_pago}</p>
-                </div>
-                {/* boton */}
-                {ele.isActive && (
+                  {/*  fecha */}
                   <div className={style.itemDiv}>
-                    <Button
-                      onClick={() => {
-                        setSuccess(true);
-                        putDesactiveColegio(ele.id);
-                      }}
-                      variant="outlined"
-                      sx={{ fontWeight: "600", fontFamily: "Poppins" }}
-                    >
-                      Desactivar
-                    </Button>
+                    <p>12 de abril del 2023</p>
                   </div>
-                )}
-                {ele.isActive === false && (
+                  {/*  plan */}
                   <div className={style.itemDiv}>
-                    <Button
-                      onClick={() => {
-                        setSuccess(true);
-                        putActiveColegio(ele.id);
-                      }}
-                      variant="outlined"
-                      sx={{ fontWeight: "600", fontFamily: "Poppins" }}
-                    >
-                     Ativar
-                    </Button>
+                    {/* //   Plan_Pago: { id: 1, nombre_plan_pago: 'Free' }, */}
+                    <p> {ele.Plan_Pago.nombre_plan_pago}</p>
                   </div>
-                )}
-              </div>
-            </>
-          );
-        })}
+                  {/* boton */}
+
+                  <ActDesButton
+                    // onClick={() => {
+                    //   putActiveColegio(ele.id);
+                    // }}
+                    isActive={ele.isActive}
+                    id={ele.id}
+                  />
+                </div>
+              </>
+            );
+          })}
       </div>
     </>
   );
