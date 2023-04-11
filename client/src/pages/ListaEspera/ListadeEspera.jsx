904f551408c6d22359a45d8361cecc9cd16576b7
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2023-04-11 00:02:44 -0300

    css lista de espera

--- client/src/pages/ListaEspera/ListadeEspera.jsx
+++ client/src/pages/ListaEspera/ListadeEspera.jsx
@@ -1,5 +1,5 @@
 import { Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./List.module.css";
 import NorthIcon from "@mui/icons-material/North";
 import ContentPasteSearchOutlinedIcon from "@mui/icons-material/ContentPasteSearchOutlined";
@@ -15,21 +15,25 @@ import {
 } from "@mui/material";
 import sliceIntoChunks from "../../components/CardsCitas/Paginacion/utils/SliceCitas";
 import axios from "axios";
+import { useSelector } from "react-redux";
 export default function ListadeEspera() {
+  const { grados } = useSelector((state) => state.schools);
   const [orderSelected, setOrderSelected] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [dataFiltrada, setDataFiltrada] = useState([]);
+
   const handleChangeState = (event) => {
     let state = event.target.value;
     setOrderSelected(state);
-
+    const ColegioId = localStorage.getItem("ColegioId");
+    console.log(ColegioId);
     try {
       setIsLoading(true);
       const token = localStorage.getItem("token");
-      const ColegioId = localStorage.getItem("ColegioId");
+
       // ?order=${state}
       axios
-        .get(`/listaDeEspera/colegio/${ColegioId}`)
+        .get(`/lista/colegio/${ColegioId}`)
         .then((res) => {
           setDataFiltrada(res.data);
           // const eventosPaginados = sliceIntoChunks(res.data, 10);
@@ -52,6 +56,37 @@ export default function ListadeEspera() {
     }
   };
 
+  useEffect(() => {
+    try {
+      setIsLoading(true);
+      const token = localStorage.getItem("token");
+      const ColegioId = localStorage.getItem("ColegioId");
+      console.log(ColegioId);
+      // ?order=${state}
+      axios
+        .get(`/lista/colegio/${ColegioId}`)
+        .then((res) => {
+          setDataFiltrada(res.data);
+          // const eventosPaginados = sliceIntoChunks(res.data, 10);
+          // setDataFiltrada(eventosPaginados);
+          // setIsLoading(false);
+        })
+        .catch((err) => {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: err.response.data.error,
+          });
+        });
+    } catch (err) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: err.message,
+      });
+    }
+  }, []);
+
   console.log(dataFiltrada);
   return (
     <>
@@ -60,49 +95,45 @@ export default function ListadeEspera() {
           Lista de Espera
         </Typography>
       </div>
-<div 
-  style={{
-    width: "80%",
-    display: "flex",
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    flexDirection: "row",
-    color: "#0C2B42",
-    gap: "10px",
- 
-    backgroundColor:'transparent',
-  }}
-
->
-  <p  style={{
-    paddingTop:'1.1vh',
-   fontSize:'1.6vh'
-  }}>Ordenar por </p>
-    <FormControl variant="standard" sx={{ m: 1, minWidth: 100 }} size="small">
-        <InputLabel id="demo-select-small">Fecha</InputLabel>
-
-        <Select
-          sx={{ border: "none", outline: "none", fontSize: "2vh" }}
-          labelId="demo-select-small"
-          id="demo-select-small"
-          // value={orderSelected}
-          label={"Fecha"}
-          onChange={handleChangeState}
+      <div
+        className={style.divFiltro}
+      >
+        <p
+          style={{
+            paddingTop: "1.1vh",
+            fontSize: "1.6vh",
+          }}
         >
-          <MenuItem value="ASC">
-            {" "}
-            Fecha <NorthIcon sx={{ width: "2vh" }} />{" "}
-          </MenuItem>
-          <MenuItem value="DESC">
-            {" "}
-            Fecha <SouthIcon sx={{ width: "2vh" }} />{" "}
-          </MenuItem>
-        </Select>
-      </FormControl>
-</div>
-    
+          Ordenar por{" "}
+        </p>
+        <FormControl
+          variant="standard"
+          sx={{ m: 1, minWidth: 100 }}
+          size="small"
+        >
+          <InputLabel id="demo-select-small">Fecha</InputLabel>
 
-      {dataFiltrada.length === 0 ? (
+          <Select
+            sx={{ border: "none", outline: "none", fontSize: "2vh" }}
+            labelId="demo-select-small"
+            id="demo-select-small"
+            // value={orderSelected}
+            label={"Fecha"}
+            onChange={handleChangeState}
+          >
+            <MenuItem value="ASC">
+              {" "}
+              Fecha <NorthIcon sx={{ width: "2vh" }} />{" "}
+            </MenuItem>
+            <MenuItem value="DESC">
+              {" "}
+              Fecha <SouthIcon sx={{ width: "2vh" }} />{" "}
+            </MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+<div className={style.layout}>
+    {dataFiltrada.length === 0 ? (
         <div
           style={{
             width: "80%",
@@ -123,75 +154,86 @@ export default function ListadeEspera() {
           <h1>Aun no hay familias en tu lista de espera.</h1>
         </div>
       ) : (
-        <div className={style.container}>
-          <div
-            style={{
-              display: "flex",
-              gap: "5px",
-              flexDirection: "column",
-              width: "100%",
-              fontSize: "1.8vh",
-            }}
-          >
-            <div
-              style={{
-                display: "flex",
-                gap: "10px",
-                flexDirection: "row",
-                alignItems: "center",
-                fontSize: "1.8vh",
-              }}
-            >
-              <img
-                style={{ width: "50px", height: "50px" }}
-                src="https://res.cloudinary.com/dj8p0rdxn/image/upload/v1676414550/xuj9waxpejcnongvhk9o.png"
-                alt=""
-              />
-              <div>
-                <div className={style.divNombreGrado}>
-                  <p>Nombre</p>
-                </div>
-              </div>
-            </div>
-          </div>
+        dataFiltrada.map((ele) => {
+          const gradoDeEspera = grados?.find((g) => g.id === ele.GradoId);
+          console.log(gradoDeEspera);
+          return (
+            <>
+              <div className={style.container}>
+          
+                  <div
+                    style={{
+                      display: "flex",
+                      gap: "10px",
+                      flexDirection: "row",
+                      alignItems: "center",
+                      fontSize: "1.8vh",
+                    }}
+                  >
+                    <img
+                      style={{ width: "50px", height: "50px" }}
+                      src="https://res.cloudinary.com/dj8p0rdxn/image/upload/v1676414550/xuj9waxpejcnongvhk9o.png"
+                      alt=""
+                    />
+                  </div>
+            
 
-          <div
-            style={{
-              display: "flex",
-              gap: "5px",
-              width: "100%",
-              justifyContent: "space-around",
-              flexDirection: "row",
-            }}
-          >
-            <div className={style.itemDiv}>
-              <p>
-                <b>Telefono</b>{" "}
-              </p>
-              <p>31234535</p>
-            </div>
-            <div className={style.itemDiv}>
-              <p>
-                {" "}
-                <b>Grado </b>
-              </p>
-              <p>1ro-Primaria</p>
-            </div>
-            <div className={style.itemDiv}>
-              <p>
-                <b>Email</b>{" "}
-              </p>
-              <p>sad@gmaiil.com</p>
-            </div>
-            <div className={style.itemDiv}>
-              <p>
-                <b>Fecha</b>
-              </p>
-              <p>22 de abril del 2023</p>
-            </div>
-          </div>
-        </div>
+                {/* <div
+                  style={{
+                    display: "flex",
+                    gap: "5px",
+                    width: "100%",
+                    justifyContent: "space-around",
+                    flexDirection: "row",
+                  }}
+                > */}
+                  <div
+                    className={style.divInfo}
+                  >
+                    <div>
+                      <div className={style.divNombreGrado}>
+                        <p>Nombre</p>
+                      </div>
+                      <div
+                        className={style.divDatos}
+                      >
+                        <div className={style.itemDiv}>
+                          <p>
+                            <b>Telefono</b>{" "}
+                          </p>
+                          <p>31234535</p>
+                        </div>
+                        <div className={style.itemDiv}>
+                          <p>
+                            {" "}
+                            <b>Grado </b>
+                          </p>
+
+                          <p>{gradoDeEspera.nombre_grado}</p>
+                        </div>
+                        <div className={style.itemDiv}>
+                          <p>
+                            <b>Email</b>{" "}
+                          </p>
+                          <p>sad@gmaiil.com</p>
+                        </div>
+                        <div className={style.itemDiv}>
+                          <p>
+                            <b>Fecha</b>
+                          </p>
+                          <p>{ele.createdAt}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              {/* </div> */}
+            </>
+          );
+        })
       )}
+</div>
+    
     </>
   );
 }
