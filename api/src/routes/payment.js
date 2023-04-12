7899d1a2a1ca748eb9c3a0736255ad028a949ed9
Robmargar <roberto.mar.garc@gmail.com>
2023-04-11 23:03:42 -0500

    pagos

--- api/src/routes/payment.js
+++ api/src/routes/payment.js
@@ -3,7 +3,7 @@ const mercadopago = require("mercadopago");
 const router = Router();
 const { Colegio, Ventas, Plan_Pago } = require("../db.js");
 const payController = require("../controllers/payController");
-const mailer = require('../utils/sendMails/mailer');
+const mailer = require("../utils/sendMails/mailer");
 
 router.get("/success", (req, res) => {
   res.send("Todo bien al 100");
@@ -38,7 +38,6 @@ router.post("/notification", async (req, res) => {
 
       if (ventas && ventas.status === "Pending") {
         if (merchantOrder.body.payments[0].status === "approved") {
-
           ventas.status = "Paid";
           ventas.mp_payment_id = paymentId;
           ventas.InicioPlan = merchantOrder.body.payments[0].date_approved;
@@ -88,7 +87,11 @@ router.post("/notification", async (req, res) => {
           // AQUI SE MANDA  EL CORREO DE QUE SE COMPRO EXITOSAMENTE <-----------------------
           // la informacion la optienes de colegio.nombre_colegio """colegio.nombre del plan"""
           // planNombre
-          mailer.sendPaymentSuccess(email,colegio.nombre_colegio, colegio.PlanPagoId);
+          mailer.sendPaymentSuccess(
+            email,
+            colegio.nombre_colegio,
+            colegio.PlanPagoId
+          );
           res.status(200).send(merchantOrder);
         }
       } else if (merchantOrder.body.payments[0].status === "canceled") {
@@ -97,7 +100,11 @@ router.post("/notification", async (req, res) => {
         await ventas.save();
         // AQUI SE MANDA  EL CORREO DE QUE SE CANCELO LA COMPRA <-----------------------
         // la informacion la optienes de colegio.nombre_colegio """colegio.nombre del plan"""
-        mailer.sendPaymentCanceled(email,colegio.nombre_colegio, colegio.PlanPagoId);
+        mailer.sendPaymentCanceled(
+          email,
+          colegio.nombre_colegio,
+          colegio.PlanPagoId
+        );
         res.status(200).send(merchantOrder);
       }
 
